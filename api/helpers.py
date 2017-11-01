2cc0eb48456713104dfe32bce2de1ca699b43ed6
Daniel Poon <daniel.poon.wenjie@gmail.com>
2017-11-01 23:12:47 +0800

    Refactor to allow general use case for `predict` route - additional predictors added in the future would automatically be discoverable

---
+++ api/helpers.py
@@ -0,0 +1,36 @@
+from flask_restful import reqparse
+
+from app import cache
+import mlmodels.predictors
+
+
+def init_request_parser(predictor):
+    parser = reqparse.RequestParser(bundle_errors=True)
+
+    for column_name, column_type in predictor.get_column_types().items():
+        if column_name != predictor.prediction_field:
+            parser.add_argument(
+                column_name,
+                type=column_type,
+                required=True,
+                location='args'
+            )
+
+    return parser
+
+
+@cache.memoize()
+def get_predictor(name, version):
+    klass_name = snake_to_camel(name)
+    predictor_klass = getattr(mlmodels.predictors, klass_name)
+
+    predictor = predictor_klass(version=version)
+    predictor.load()
+
+    print(f'{klass_name} v{version} loaded')
+
+    return predictor
+
+
+def snake_to_camel(input_text):
+    return ''.join(x.capitalize() for x in input_text.split('_'))
--- api/v1/routes.py
+++ api/v1/routes.py
@@ -1,50 +1,41 @@
-from flask_restful import Resource, reqparse
+from flask import jsonify, make_response
+from flask_restful import Resource
 
 from api import flask_api
-from app import cache
+from api.helpers import init_request_parser, get_predictor
 
-from mlmodels.predictors import WineQuality
-
-API_VERSION = 'v1'
-ROUTE_PREFIX = f'/api/{API_VERSION}'
+API_VERSION = 1
+ROUTE_PREFIX = f'/api/v{API_VERSION}'
 
 
 class Heartbeat(Resource):
     def get(self):
-        return {
-            'status': 'ok'
-        }
+        return make_response(jsonify({'status': 'ok'}), 200)
 
 
-class WineQualityResource(Resource):
-    def get(self):
-        predictor = get_predictor()
+class Predictor(Resource):
+    def get(self, predictor_name):
+        try:
+            predictor = get_predictor(predictor_name, API_VERSION)
+        except AttributeError:
+            return make_response(jsonify({'message': f'{predictor_name} not found'}), 404)
 
-        parser = reqparse.RequestParser(bundle_errors=True)
-        for column_name, column_type in predictor.get_column_types().items():
-            if column_name != predictor.prediction_field:
-                parser.add_argument(
-                    column_name,
-                    type=column_type,
-                    required=True,
-                    location='args'
-                )
-
+        parser = init_request_parser(predictor)
         parsed_args = parser.parse_args(strict=True)
         result = predictor.predict(parsed_args)
 
-        return {
-            'field': predictor.prediction_field,
-            'result': result,
-            'version': predictor.clf_version
-        }
+        return make_response(
+            jsonify({
+                'field': predictor.prediction_field,
+                'result': result,
+                'version': predictor.clf_version
+            }), 200
+        )
 
+flask_api.add_resource(Heartbeat, f'{ROUTE_PREFIX}/heartbeat', endpoint=f'api.v{API_VERSION}.heartbeat')
 
-@cache.cached(key_prefix='wine_quality')
-def get_predictor():
-    predictor = WineQuality(version=1)
-    predictor.load()
-    return predictor
-
-flask_api.add_resource(Heartbeat, f'{ROUTE_PREFIX}/heartbeat', endpoint='api.v1.heartbeat')
-flask_api.add_resource(WineQualityResource, f'{ROUTE_PREFIX}/predict/wine_quality', endpoint='api.v1.predict.wine_quality')
+flask_api.add_resource(
+    Predictor,
+    f'{ROUTE_PREFIX}/predict/<string:predictor_name>',
+    endpoint=f'api.v{API_VERSION}.predict'
+)

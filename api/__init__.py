59912c1a2071eb14eb9aea346025c0c712e5d8c9
Daniel Poon <daniel.poon.wenjie@gmail.com>
2017-11-02 00:37:04 +0800

    Fix: error due to import order

--- api/__init__.py
+++ api/__init__.py
@@ -4,3 +4,8 @@ from app import flask_app
 flask_api = Api(flask_app)
 
 from api.v1.routes import flask_api
+
+from api.helpers import get_predictor
+
+# cache predictor prefork
+get_predictor('wine_quality', 1)
--- app/__init__.py
+++ app/__init__.py
@@ -2,15 +2,10 @@ from flask import Flask
 from flask_caching import Cache
 from dotenv import load_dotenv, find_dotenv
 
-from api.helpers import get_predictor
-
-flask_app = Flask(__name__)
-cache = Cache(flask_app, config={'CACHE_TYPE': 'simple'})
-
 # load environment variables
 load_dotenv(find_dotenv())
 
-# cache predictor prefork
-get_predictor('wine_quality', 1)
+flask_app = Flask(__name__)
+cache = Cache(flask_app, config={'CACHE_TYPE': 'simple'})
 
 from api import flask_app

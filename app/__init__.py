9492e648ec4e80e70ace3c449526d804e5f980f1
Daniel Poon <daniel.poon.wenjie@gmail.com>
2017-11-01 23:13:34 +0800

    Load predictor on startup (prefork) to prevent initial cold cache hit

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,15 @@ from flask import Flask
 from flask_caching import Cache
 from dotenv import load_dotenv, find_dotenv
 
-# load environment variables
-load_dotenv(find_dotenv())
+from api.helpers import get_predictor
 
 flask_app = Flask(__name__)
 cache = Cache(flask_app, config={'CACHE_TYPE': 'simple'})
+
+# load environment variables
+load_dotenv(find_dotenv())
+
+# cache predictor prefork
+get_predictor('wine_quality', 1)
 
 from api import flask_app
